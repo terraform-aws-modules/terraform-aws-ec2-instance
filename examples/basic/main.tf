ee9c7ad1ade2e222adc84ec8208343a73f0c4d60
Anton Babenko <anton@antonbabenko.com>
2017-10-14 12:46:35 +0200

    Updated example with all-icmp security group rule

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -40,8 +40,9 @@ module "security_group" {
   description = "Security group for example usage with EC2 instance"
   vpc_id      = "${data.aws_vpc.default.id}"
 
-  ingress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_rules            = ["http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
 }
 
 module "ec2" {
