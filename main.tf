403ddf4b52ade45c79a50c095a2f0c29315a62ab
Dani Hodovic <danihodovic@users.noreply.github.com>
2018-09-04 12:11:25 +0200

    Ignore changes in the ebs_block_device (#46)
    Any change in the ebs block device currently triggers re-creation of
    this resource which I assume is not intended.
    
    Workaround by ignoring these changes as suggested in:
    
    https://github.com/terraform-providers/terraform-provider-aws/issues/1297#issuecomment-319359965

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "this" {
     # Due to several known issues in Terraform AWS provider related to arguments of aws_instance:
     # (eg, https://github.com/terraform-providers/terraform-provider-aws/issues/2036)
     # we have to ignore changes in the following arguments
-    ignore_changes = ["private_ip", "root_block_device"]
+    ignore_changes = ["private_ip", "root_block_device", "ebs_block_device"]
   }
 }
 
@@ -83,6 +83,6 @@ resource "aws_instance" "this_t2" {
     # Due to several known issues in Terraform AWS provider related to arguments of aws_instance:
     # (eg, https://github.com/terraform-providers/terraform-provider-aws/issues/2036)
     # we have to ignore changes in the following arguments
-    ignore_changes = ["private_ip", "root_block_device"]
+    ignore_changes = ["private_ip", "root_block_device", "ebs_block_device"]
   }
 }
