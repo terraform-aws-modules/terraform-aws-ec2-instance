aeeb94e856c7b0250e537ab9617fabf43158c8fb
Marc Chamberland <mchamberland@pbsc.com>
2018-10-19 13:51:24 -0400

    support for Chef provisioner

--- main.tf
+++ main.tf
@@ -22,6 +22,59 @@ resource "aws_instance" "this" {
   ipv6_address_count          = "${var.ipv6_address_count}"
   ipv6_addresses              = "${var.ipv6_addresses}"
 
+  # Make sure Chef waits for cloud-init before executing
+  # While remote-exec could replace cloud-init here, it cannot be used with autoscaling groups (among others)
+  # As such usage of remote-exec should be minimized so we do not end up depending on it
+  provisioner "remote-exec" {
+    inline = [
+      "while true; do",
+      "if ! [ -e /var/lib/cloud/instance/boot-finished ]; then",
+      "sleep 10",
+      "else",
+      "exit 0",
+      "fi; done",
+    ]
+    connection {
+      type                   = "ssh"
+      user                   = "${var.default_system_user}"
+      private_key            = "${var.ssh_private_key}"
+
+      # Some nodes require connecting via a Bastion instance
+      bastion_host           = "${var.bastion_host}"
+      bastion_user           = "${var.bastion_user}"
+      bastion_private_key    = "${var.bastion_private_key}"
+    }
+  }
+
+  provisioner "chef" {
+    environment              = "${var.chef_environment}"
+    # We're imposing a single role as the run-list as part of our in-house standards
+    run_list                 = ["role[${var.chef_role}]"]
+    node_name                = "${var.chef_role}-${count.index}"
+    secret_key               = "${var.chef_secret_key}"
+    server_url               = "${var.chef_server_url}/organizations/${var.chef_organization}"
+    fetch_chef_certificates  = true
+    recreate_client          = true
+    user_name                = "${var.chef_user}"
+    user_key                 = "${var.chef_user_key}"
+    vault_json               = "${var.chef_vault_json}"
+    version                  = "${var.chef_client_version}"
+    ssl_verify_mode          = ":verify_peer"
+    log_to_file              = true
+    disable_reporting        = true
+
+    connection {
+      type                   = "ssh"
+      user                   = "${var.default_system_user}"
+      private_key            = "${var.ssh_private_key}"
+
+      # Some nodes require connecting via a Bastion instance
+      bastion_host           = "${var.bastion_host}"
+      bastion_user           = "${var.bastion_user}"
+      bastion_private_key    = "${var.bastion_private_key}"
+    }
+  }
+
   ebs_optimized          = "${var.ebs_optimized}"
   volume_tags            = "${var.volume_tags}"
   root_block_device      = "${var.root_block_device}"
@@ -60,6 +113,61 @@ resource "aws_instance" "this_t2" {
   private_ip                  = "${var.private_ip}"
   ipv6_address_count          = "${var.ipv6_address_count}"
   ipv6_addresses              = "${var.ipv6_addresses}"
+
+  # Make sure Chef waits for cloud-init before executing
+  # While remote-exec could replace cloud-init here, it cannot be used with autoscaling groups (among oth
+ers)
+  # As such usage of remote-exec should be minimized so we do not end up depending on it
+
+  provisioner "remote-exec" {
+    inline = [
+      "while true; do",
+      "if ! [ -e /var/lib/cloud/instance/boot-finished ]; then",
+      "sleep 10",
+      "else",
+      "exit 0",
+      "fi; done",
+    ]
+    connection {
+      type                   = "ssh"
+      user                   = "${var.default_system_user}"
+      private_key            = "${var.ssh_private_key}"
+      timeout                = "10m"
+      # Some nodes require connecting via a Bastion instance
+      bastion_host           = "${var.bastion_host}"
+      bastion_user           = "${var.bastion_user}"
+      bastion_private_key    = "${var.bastion_private_key}"
+    }
+  }
+
+  provisioner "chef" {
+    environment              = "${var.chef_environment}"
+    # We're imposing a single role as the run-list as part of our in-house standards
+    run_list                 = ["role[${var.chef_role}]"]
+    node_name                = "${var.chef_role}-${count.index}"
+    secret_key               = "${var.chef_secret_key}"
+    server_url               = "${var.chef_server_url}/organizations/${var.chef_organization}"
+    fetch_chef_certificates  = true
+    recreate_client          = true
+    user_name                = "${var.chef_user}"
+    user_key                 = "${var.chef_user_key}"
+    vault_json               = "${var.chef_vault_json}"
+    version                  = "${var.chef_client_version}"
+    ssl_verify_mode          = ":verify_peer"
+    log_to_file              = true
+    disable_reporting        = true
+
+    connection {
+      type                   = "ssh"
+      user                   = "${var.default_system_user}"
+      private_key            = "${var.ssh_private_key}"
+      timeout                = "10m"
+      # Some nodes require connecting via a Bastion instance
+      bastion_host           = "${var.bastion_host}"
+      bastion_user           = "${var.bastion_user}"
+      bastion_private_key    = "${var.bastion_private_key}"
+    }
+  }
 
   ebs_optimized          = "${var.ebs_optimized}"
   volume_tags            = "${var.volume_tags}"
