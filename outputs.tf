b637021c012ad46a24bc3f2c2cbdb16886445d27
Anton Babenko <anton@antonbabenko.com>
2019-06-08 16:25:56 +0200

    Added support for placement group and volume tags (#96)

--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,13 @@ locals {
   this_primary_network_interface_id = compact(coalescelist(aws_instance.this.*.primary_network_interface_id, aws_instance.this_t2.*.primary_network_interface_id, [""]))
   this_private_dns                  = compact(coalescelist(aws_instance.this.*.private_dns, aws_instance.this_t2.*.private_dns, [""]))
   this_private_ip                   = compact(coalescelist(aws_instance.this.*.private_ip, aws_instance.this_t2.*.private_ip, [""]))
+  this_placement_group              = compact(concat(coalescelist(aws_instance.this.*.placement_group, aws_instance.this_t2.*.placement_group), [""]))
   this_security_groups              = coalescelist(aws_instance.this.*.security_groups, aws_instance.this_t2.*.security_groups, [""])
   this_vpc_security_group_ids       = coalescelist(flatten(aws_instance.this.*.vpc_security_group_ids), flatten(aws_instance.this_t2.*.vpc_security_group_ids), [""])
   this_subnet_id                    = compact(coalescelist(aws_instance.this.*.subnet_id, aws_instance.this_t2.*.subnet_id, [""]))
-  this_credit_specification         = aws_instance.this_t2.*.credit_specification
+  this_credit_specification         = flatten(aws_instance.this_t2.*.credit_specification)
   this_tags                         = coalescelist(aws_instance.this.*.tags, aws_instance.this_t2.*.tags, [""])
+  this_volume_tags                  = coalescelist(aws_instance.this.*.volume_tags, aws_instance.this_t2.*.volume_tags, [""])
 }
 
 output "id" {
@@ -24,11 +26,10 @@ output "availability_zone" {
   value       = local.this_availability_zone
 }
 
-// GH issue: https://github.com/terraform-aws-modules/terraform-aws-ec2-instance/issues/8
-//output "placement_group" {
-//  description = "List of placement groups of instances"
-//  value       = ["${element(concat(aws_instance.this.*.placement_group, list("")), 0)}"]
-//}
+output "placement_group" {
+  description = "List of placement groups of instances"
+  value       = local.this_placement_group
+}
 
 output "key_name" {
   description = "List of key names of instances"
@@ -85,3 +86,7 @@ output "tags" {
   value       = local.this_tags
 }
 
+output "volume_tags" {
+  description = "List of tags of volumes of instances"
+  value       = local.this_volume_tags
+}
