9fcea6189959ccb0b234cdf926a3948e60ca46e8
Tony Nyurkin <tony@simplepractice.com>
2020-01-02 13:33:56 +0200

    Add ignore changes of key_name

--- main.tf
+++ main.tf
@@ -93,4 +93,10 @@ resource "aws_instance" "this" {
   credit_specification {
     cpu_credits = local.is_t_instance_type ? var.cpu_credits : null
   }
+
+  lifecycle {
+    ignore_changes = [
+      key_name
+    ]
+  }
 }
