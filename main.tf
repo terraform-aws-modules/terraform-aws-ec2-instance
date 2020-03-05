ed925f6796dabf13a5eedf17e43f65a0844d6b5d
Claudio Iabichino <ciabichino@tierratelematics.com>
2020-03-05 17:25:36 +0100

    Updated t instance type check to include t3a type

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  is_t_instance_type = "${replace(var.instance_type, "/^t[23]{1}\\..*$/", "1") == "1" ? "1" : "0"}"
+  is_t_instance_type = "${replace(var.instance_type, "/^t(2|3|3a){1}\\..*$/", "1") == "1" ? "1" : "0"}"
 }
 
 ######
