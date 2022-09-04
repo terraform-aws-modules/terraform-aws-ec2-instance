5cc414b1979774244087d51dd72a424e5ba1a5ae
John Schutz <328434+tofupup@users.noreply.github.com>
2022-09-04 21:49:30 +0000

    Add ec2_t4g_standard test outputs

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -136,6 +136,52 @@ output "ec2_t3_unlimited_tags_all" {
   value       = module.ec2_t3_unlimited.tags_all
 }
 
+# EC2 t4g standard
+output "ec2_t4g_standard_id" {
+  description = "The ID of the instance"
+  value       = module.ec2_t4g_standard.id
+}
+
+output "ec2_t4g_standard_arn" {
+  description = "The ARN of the instance"
+  value       = module.ec2_t4g_standard.arn
+}
+
+output "ec2_t4g_standard_capacity_reservation_specification" {
+  description = "Capacity reservation specification of the instance"
+  value       = module.ec2_t4g_standard.capacity_reservation_specification
+}
+
+output "ec2_t4g_standard_instance_state" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = module.ec2_t4g_standard.instance_state
+}
+
+output "ec2_t4g_standard_primary_network_interface_id" {
+  description = "The ID of the instance's primary network interface"
+  value       = module.ec2_t4g_standard.primary_network_interface_id
+}
+
+output "ec2_t4g_standard_private_dns" {
+  description = "The private DNS name assigned to the instance. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
+  value       = module.ec2_t4g_standard.private_dns
+}
+
+output "ec2_t4g_standard_public_dns" {
+  description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
+  value       = module.ec2_t4g_standard.public_dns
+}
+
+output "ec2_t4g_standard_public_ip" {
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  value       = module.ec2_t4g_standard.public_ip
+}
+
+output "ec2_t4g_standard_tags_all" {
+  description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block"
+  value       = module.ec2_t4g_standard.tags_all
+}
+
 # EC2 Multiple
 output "ec2_multiple" {
   description = "The full output of the `ec2_module` module"
