f63caf03d8d36db5d59712b72f03b6f31973ebdd
jebbens <jebbens@amazon.com>
2023-04-15 16:57:38 +0000

    add tags to ebs_block_device

--- main.tf
+++ main.tf
@@ -87,6 +87,7 @@ resource "aws_instance" "this" {
       volume_size           = lookup(ebs_block_device.value, "volume_size", null)
       volume_type           = lookup(ebs_block_device.value, "volume_type", null)
       throughput            = lookup(ebs_block_device.value, "throughput", null)
+      tags                  = lookup(ebs_block_device.value, "tags", null)
     }
   }
 
