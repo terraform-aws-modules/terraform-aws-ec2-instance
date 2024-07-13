e458780a3565415ee8c3def7222f36185e5a3c62
zorowa <b.opokuyeboah@gmail.com>
2024-07-13 20:56:50 +0000

    this arranges folders

---
+++ zoro.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Author: Bediako
+
+touch b2.sh bashass.sh bash.sh black.mp3 bubu.avi dame.mov david.flac dev.log dino.log edochie.jpg efe.log games.avi hunger.mov ampple.jnp naija.jpg nana.mov peache/py pete/png pytho.py shata.flac sheriff.flac toyin.pdf van.avi wale.mp3 wike.avi wiz.mp3 yul.jpg yvonne.txt zanku.mp3 ghana.avi
+
+mkdir music video images videos
+mv *.mp3 *.flac music
+mv *.jpg *.png images
+mv *.avi *.mov videos
+rm -rvf *.log
+
+tree
+
