45a43803c408968f9f9d11b2ea63d96f6931a1e2
Artem Hatchenko <102317797+artem-hatchenko@users.noreply.github.com>
2025-08-22 19:09:35 +0300

    feat: Output security group ARN and ID (#450)
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -230,3 +230,17 @@ output "ephemeral_block_device" {
     null
   )
 }
+
+################################################################################
+# Security Group
+################################################################################
+
+output "security_group_arn" {
+  description = "Amazon Resource Name (ARN) of the security group"
+  value       = try(aws_security_group.this[0].arn, null)
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = try(aws_security_group.this[0].id, null)
+}
